b7ba90d1a35d007c1e688c47a1684735ed7df8e7
SULTAN <ramhziworld@gmail.com>
2020-05-07 19:39:37 +0100

    Updated markers

--- app/src/main/java/com/example/mymaps/MapsActivity.java
+++ app/src/main/java/com/example/mymaps/MapsActivity.java
@@ -8,12 +8,28 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng mountEverest = new LatLng(27.990203, 86.925232);
+    private LatLng olumoRock = new LatLng(7.230583, 3.438459);
+//    private  LatLng mountEverest= new LatLng(27.990203, 86.925232);
+
+    private Marker everest;
+    private Marker olumo;
+
+    private MarkerOptions everestOptions;
+    private  MarkerOptions olumoOptions;
+
+    private ArrayList<Marker> markers;
+    private ArrayList<MarkerOptions>markerOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +54,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+//        mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
 
+        markers = new ArrayList<>();
+        markerOptions = new ArrayList<>();
+
+        everestOptions = new MarkerOptions()
+                .position(mountEverest)
+                .title("Mount Evrest")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        markers.add(everest);
+        markerOptions.add(everestOptions);
+
+        olumoOptions = new MarkerOptions()
+                .position(olumoRock)
+                .title("Olumo rock")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+        markers.add(olumo);
+        markerOptions.add(olumoOptions);
+
+//        LatLng binga = new LatLng(-19.7766658, 33.0444344);
+//        markers.add(everest);
+
+        for (MarkerOptions m:markerOptions)
+        {
+            LatLng latLng = new LatLng(m.getPosition().latitude,m.getPosition().longitude);
+            mMap.addMarker(m);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,4));
+        }
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//        mMap.addMarker(new MarkerOptions().position(binga).title("Mount binga")
+//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(markers.get));
     }
 }
